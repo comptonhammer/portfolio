ed21244046293747f860e571258a619391885a67
comptonhammer <34519622+comptonhammer@users.noreply.github.com>
2020-07-30 11:09:17 -0700

    Update settings.js

--- nodejs/predictive_lead_scoring/public/js/settings.js
+++ nodejs/predictive_lead_scoring/public/js/settings.js
@@ -1,13 +1,14 @@
-window.onload = function() {
-    var model = $('#model').attr('value');
+window.onload = () => {
+    let model = $('#model').attr('value');
     if(model != 'Custom'){
         $('[name=model]').attr('value', model);
-        $('select[name="model"]').find(`option:contains("${model}")`).attr('selected',true);
+        $('select[name="model"]').find(`option:contains("${model}")`).attr('selected', true);
     }
 };
 
 $("#save").click(function(e) {
     e.preventDefault();
+    
     if($("#save").html() == 'Save') 
         $("#save").html('Are You Sure?')
     else if($("#save").html() == 'Are You Sure?') 
@@ -15,7 +16,8 @@ $("#save").click(function(e) {
     else {
         $("#save").html('Saving...');
         $("#save").css("background-color","red");
-        const formData = $('#fullBody').serialize();
+        const formData = $('#full-body').serialize();
+        
         $.post('/settings/update', formData, status => {
             if(status.errMsg) 
                 $("#save").html(status.errMsg);
@@ -27,8 +29,9 @@ $("#save").click(function(e) {
     }
 });
 
-$("#backBtn").click(function(e) {
+$("#back-btn").click(function(e) {
     e.preventDefault();
+    
     window.location.replace('/home');
     return false;
 });
