e1784d3e42a9c875caa8e24088a6d5d220cfbf38
comptonhammer <34519622+comptonhammer@users.noreply.github.com>
2020-07-30 11:07:35 -0700

    Update login.js

--- nodejs/predictive_lead_scoring/public/js/login.js
+++ nodejs/predictive_lead_scoring/public/js/login.js
@@ -1,18 +1,21 @@
 $('#iForm').submit(function(e){
     e.preventDefault();
-    const formData = $('#iForm').serialize();
+    
+    const formData = $('#i-form').serialize();
+    
     $.post('/login', formData)
-        .done((res) => {
-            if (res != false) window.location.replace(res.url)
+        .done(res => {
+            if(res) 
+                window.location.replace(res.url)
             else {
                 document.getElementById("password").style.backgroundColor = "pink";
-                document.getElementById("errorDef").innerHTML = "Hmm, we couldn't find that account, or the password is wrong. Give it another try or <a href='https://leadjuju.com/support'>contact us</a>."
+                document.getElementById("error-def").innerHTML = "Hmm, we couldn't find that account, or the password is wrong. Give it another try or <a href='https://leadjuju.com/support'>contact us</a>."
             }
         })
-        .fail((xhr) => {
+        .fail(xhr => {
             if(xhr.status == 429){
                 document.getElementById("password").style.backgroundColor = "pink";
-                document.getElementById("errorDef").innerHTML = "You've had too many login attempts. Wait and try again."
+                document.getElementById("error-def").innerHTML = "You've had too many login attempts. Wait and try again."
             }
         })
 });
