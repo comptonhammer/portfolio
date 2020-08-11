6f22041354d2b3fa08cc51d8593d2758ab578182
comptonhammer <34519622+comptonhammer@users.noreply.github.com>
2020-08-11 14:08:48 -0700

    Update unsub.js

--- nodejs/predictive_lead_scoring/public/js/unsub.js
+++ nodejs/predictive_lead_scoring/public/js/unsub.js
@@ -12,14 +12,14 @@ function unsub(){
         if (data && data.active) 
             window.location.replace(data.url);
         else if(!data.active){
-            document.getElementById("errorDef").innerHTML = "Hmm, looks like that subscription is already inactive/cancelled. <a href='https://leadjuju.com/support'>Contact us</a> if you're having any problems."
+            document.getElementById("errorDef").innerHTML = "Hmm, looks like that subscription is already inactive/cancelled. <a href='https://REDACTED.com/support'>Contact us</a> if you're having any problems."
         }
         else if(data.err){
             document.getElementById("errorDef").innerHTML = data.errMsg;
         }
         else {
             document.getElementById("password").style.backgroundColor = "pink";
-            document.getElementById("errorDef").innerHTML = "Hmm, we couldn't find that account, or the password is wrong. Give it another try or <a href='https://leadjuju.com/support'>contact us</a>."
+            document.getElementById("errorDef").innerHTML = "Hmm, we couldn't find that account, or the password is wrong. Give it another try or <a href='https://REDACTED.com/support'>contact us</a>."
         }
     });
 }
